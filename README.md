5257ad34a2622a13d1424fbff4575ff69ad8ba0f
Abir Ghosh <64020453+Kalihackz@users.noreply.github.com>
2020-12-16 23:45:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# videoApp
+# VRex-VideoChatApp
+Link: https://vrex-videoapp.herokuapp.com/
+
+This is a video chat app using NodeJs, socketio and webRTC.
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server)
+const {v4:uuidv4} = require('uuid');
+const {ExpressPeerServer} = require('peer');
+const peerServer = ExpressPeerServer(server,{
+    debug:true
+})
+
+app.set("view engine","ejs");
+app.use(express.static('public'));
+
+app.use('/peerjs',peerServer);
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+})
+app.get('/exit',(req,res) => {
+    res.render("exit",{});
+})
+
+app.get('/:room', (req, res) => {
+    res.render("room",{roomId: req.params.room});
+})
+
+
+
+io.on('connection',socket =>{
+    socket.on('join-room',(roomId,userId)=>{
+        console.log(`${Object.keys(io.sockets.connected).length} clients connected`);
+        io.emit('total',Object.keys(io.sockets.connected).length);
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected',userId);
+        socket.on('message',data =>{
+            io.to(roomId).emit('createMessage',data)
+        });
+
+        socket.on('disconnect', () => {
+             socket.to(roomId).broadcast.emit('user-disconnected', userId)
+             console.log(`${Object.keys(io.sockets.connected).length} clients connected`);
+            io.emit('total',Object.keys(io.sockets.connected).length);
+          })
+    })
+})
+
+server.listen(process.env.PORT||3030);
